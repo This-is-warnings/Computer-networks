73d7c1d90a09b2bef5a0245ea0034c3a1e3d420e
This-is-warnings <vboboed1@mail.ru>
2020-09-20 19:26:34 +0300

    add old lab's

---
+++ lab10Csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcp = new TcpClient("mail.ngs.ru", 110);
+            NetworkStream ns = tcp.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            string response = "GET /" + path + " HTTP/1.0\r\nHost: " + host + "\r\n\r\n";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(response);
+            ns.Write(data, 0, data.Length);
+            string message = sr.ReadToEnd();     // считываем все сообщение
+
+        }
+    }
+}
